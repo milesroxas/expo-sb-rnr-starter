2252f8b13dc00fff238912759bb9121595926c9b
Miles Roxas <132398659+sas-miles@users.noreply.github.com>
2025-09-29 20:09:07 -0400

    feat: sentry and eas update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,12 +11,32 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { AuthProvider } from "@/features/auth/providers/AuthProvider";
 import { NAV_THEME } from "@/styles/theme";
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://c993b67496005cf2368a92f96ea607e2@o4510105496584192.ingest.us.sentry.io/4510105587941376',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 LogBox.ignoreLogs(["SafeAreaView has been deprecated"]);
 
 export { ErrorBoundary } from "expo-router";
 
-export default function RootLayout() {
+export default Sentry.wrap(function RootLayout() {
   const { colorScheme } = useColorScheme();
 
   return (
@@ -39,4 +59,4 @@ export default function RootLayout() {
       </SafeAreaProvider>
     </>
   );
-}
+});
--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,10 @@
-const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+const {
+  getSentryExpoConfig
+} = require("@sentry/react-native/metro");
 
  
-const config = getDefaultConfig(__dirname);
+const config = getSentryExpoConfig(__dirname);
 
 module.exports = withNativeWind(config, {
   input: './src/styles/global.css',
